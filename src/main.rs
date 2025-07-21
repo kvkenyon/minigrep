285f1f9904f0f634dbe8dbf8114133a4f787918e
Kevin <kvkenyon@gmail.com>
2025-07-21 17:47:34 -0500

    refactor parsing logic out of main and create config struct

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,23 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filepath = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {query} in {filepath}.");
+    println!("Searching for {} in {}.", config.query, config.file_path);
 
-    let contents =
-        fs::read_to_string(filepath).expect("Should be able to read the file: {filepath}");
+    let contents = fs::read_to_string(&config.file_path)
+        .expect(format!("should be able to open: {}", config.file_path).as_str());
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
