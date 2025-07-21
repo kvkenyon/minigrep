6e17028e316a5f55c0c96deb43ee4438653df5bf
Kevin <kvkenyon@gmail.com>
2025-07-21 17:36:19 -0500

    Add test file and read logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching for {query} in {filepath}.");
+
+    let contents =
+        fs::read_to_string(filepath).expect("Should be able to read the file: {filepath}");
+
+    println!("With text:\n{contents}");
 }
